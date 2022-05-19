9bb1216a8e2b4bc2b7b7dfe2692f5ff3cec1bc2c
Jeremiah Quill <jcq5010@gmail.com>
2022-05-19 16:33:16 -0400

    added new environment variable to a local .env, removed .env from git

--- src/components/ChooseRecipe/ChooseRecipe.js
+++ src/components/ChooseRecipe/ChooseRecipe.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Button from "../Button";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 
 const ChooseRecipe = () => {
+  const [recipe, setRecipe] = useState(null);
   const navigate = useNavigate();
   const location = useLocation();
 
   function handleChooseRecipe() {
     // TODO: API call to openAI to ask for instructions on how to cook a certain recipe
-    navigate("/get-cookin");
+    setRecipe("get cookin good lookin!");
   }
+
+  useEffect(() => {
+    if (recipe !== null) {
+      navigate("/get-cookin", { state: { recipe } });
+    }
+  }, [navigate, recipe]);
 
   if (location.state === null) {
     return (
--- src/components/GetCookin/GetCookin.js
+++ src/components/GetCookin/GetCookin.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const GetCookin = () => {
-  return <div>Get Cookin</div>;
+  const location = useLocation();
+
+  if (location.state === null) {
+    return (
+      <h1>
+        Hey, you're not supposed to be here yet! Click{" "}
+        <Link className="underline text-orange-700 font-bold" to="/">
+          here
+        </Link>{" "}
+        to select ingredients.
+      </h1>
+    );
+  }
+
+  return <div>{location.state.recipe}</div>;
 };
 
 export default GetCookin;
