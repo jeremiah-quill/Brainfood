d21c906f22ca4360cfeb5e809dc94004f3fe649b
Jeremiah Quill <jcq5010@gmail.com>
2022-05-20 08:51:59 -0400

    added share social button to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { FaLightbulb } from "react-icons/fa";
+import { BsShareFill } from "react-icons/bs";
 
 const Navbar = () => {
   return (
     <div className="fixed z-10 top-0 flex items-center justify-between w-full p-4">
-      <div className="flex items-center">
+      <div className="flex items-center gap-3">
         <FaLightbulb color="#D4AF37" size="1.5rem" />
         <h1 className="text-green-600 font-thin text-3xl">AI Chef</h1>
       </div>
-      <button
-        onClick={() => console.log("clicked about")}
-        className="border-2 border-black font-bold px-2 rounded-full">
-        ?
-      </button>
+      <div className="flex gap-3">
+        <button
+          onClick={() => console.log("clicked about")}
+          className="border-2 border-black font-bold px-2 rounded-full">
+          ?
+        </button>
+        <button onClick={() => console.log("clicked social")}>
+          <BsShareFill size="1.5rem" />
+        </button>
+      </div>
     </div>
   );
 };
